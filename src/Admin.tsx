22923fbfc964dce3c83399158d94db00deef7035
Norair Baroyan <baroyannorair@gmail.com>
2026-09-17 00:53:04 +0400

    Update Admin.tsx

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { CosmicParallaxBg } from './CosmicParallaxBg';
-import { collection, addDoc, getDocs, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+import { collection, addDoc, getDocs, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
 import { db } from "./firebase";
 
 const getAdminPreviewUrl = (url: string) => {
